51a2d8d7b6af33d0e42ab5a4cb5370d49b879a35
Nuttakorn Buajarern <nuttakorn.buajarern@gmail.com>
2023-09-17 17:26:19 +0700

    add player attributes

--- Models/Player.cs
+++ Models/Player.cs
@@ -3,7 +3,11 @@ namespace Microgame.Models;
 public class Player 
 {
   public long Id { get; set; }
-  public string? Name {get; set;}
+  public string? Name { get; set;}
+  public int MaxHealthPoint { get; set;}
+  public int MaxStaminaPoint { get; set;}
+  public int CurrentHealthPoint { get; set;}
+  public int CurrentStaminaPoint { get; set;}
   public int Attack { get; set;}
   public int Defense { get; set;}
 }
